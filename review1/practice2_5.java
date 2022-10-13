602237fafa5475c698384df81b6b96c4d43d9cbd
yeopyeop-82 <a01055143647@gmail.com>
2022-10-13 20:16:03 +0900

    review

---
+++ review1/practice2_5.java
@@ -0,0 +1,12 @@
+package review1;
+import java.util.Scanner;
+
+public class practice2_5 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int time = s.nextInt();
+		int hour, minute, second;
+		
+		hour = 
+	}
+}
